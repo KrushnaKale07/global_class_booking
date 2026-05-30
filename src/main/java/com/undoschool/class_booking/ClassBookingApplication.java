3296f84d9ac9d6d4d2db2bee4294c0df5cda1049
KrushnaKale07 <krushnakale0007@gmail.com>
2026-05-30 13:25:16 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/undoschool/class_booking/ClassBookingApplication.java
@@ -0,0 +1,13 @@
+package com.undoschool.class_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/undoschool/class_booking/ClassBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undoschool.class_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
